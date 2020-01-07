26ce379e4fc48f8c1d4256d3240af58b59efd0bc
Wesley LeMahieu <wesleylemahieu@gmail.com>
2020-01-07 07:30:30 -0800

    Implements Tedis over Redis for async/await Redis operations

--- server/modules/Chat/ibm-watson/accessToken/index.js
+++ server/modules/Chat/ibm-watson/accessToken/index.js
@@ -1,9 +1,9 @@
-import redis from 'redis';
+import { Tedis } from 'tedis';
 import AuthorizationV1 from 'ibm-watson/authorization/v1.js';
 import WatsonAuth from 'ibm-watson/auth/index.js';
 import authenticator from '../authenticator/index.js';
 
-const client = redis.createClient();
+const tedis = new Tedis();
 // build authorization URL
 const instance = process.env.WATSON_TTS_INSTANCE;
 const baseUrl = process.env.WATSON_TTS_URL;
@@ -11,26 +11,15 @@ const url = `https://${baseUrl}/instances/${instance}`;
 // build authorization instantiator
 const authorization = new AuthorizationV1({ authenticator, url });
 
-export const createAccessToken = () => {
-	return new Promise((resolve, reject) => {
-		authorization.getToken((err, token) => {
-		  if (!token) {
-		    reject(err);
-		  }
-			resolve(token);
-		});
-	});
+export const createAccessToken = async () => {
+	return await authorization.getToken((err, token) => token ? token : null);
 };
 
-export const getAccessToken = () => {
-	return new Promise((resolve, reject) => {
-		client.get("WATSON_ACCESS_TOKEN", (err, token) => {
-			if (err) {
-				reject(err);
-			}
-			resolve(token.toString());
-		});
-	});
+export const getAccessToken = async () => {
+	const token = await tedis.get("WATSON_ACCESS_TOKEN");
+	return token.toString();
 };
 
-export const setAccessToken = (newToken) => client.set("WATSON_ACCESS_TOKEN", newToken);
+export const setAccessToken = async (newToken) => {
+	return await tedis.set("WATSON_ACCESS_TOKEN", newToken);
+};
